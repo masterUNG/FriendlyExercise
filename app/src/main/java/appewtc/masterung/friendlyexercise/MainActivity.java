ac6e7d0683c8cdd520571d9721760181fb13c61a
masterUNG <phrombutr@gmail.com>
2016-01-12 13:04:23 +0700

    Intent to SignUp finish

--- app/src/main/java/appewtc/masterung/friendlyexercise/MainActivity.java
+++ app/src/main/java/appewtc/masterung/friendlyexercise/MainActivity.java
@@ -1,8 +1,10 @@
 package appewtc.masterung.friendlyexercise;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         deleteAllSQLite();
 
     }   // Main Method
+
+    public void clickSignUp(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
 
     private void deleteAllSQLite() {
         SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME, MODE_PRIVATE, null);
