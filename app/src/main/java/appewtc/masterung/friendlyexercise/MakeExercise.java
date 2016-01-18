98152bf03d1a1f2f1231abafa013cfaac719d414
masterUNG <phrombutr@gmail.com>
2016-01-18 19:44:26 +0700

    อาจารย์เลือกวิชาได้แว้ววว

--- app/src/main/java/appewtc/masterung/friendlyexercise/MakeExercise.java
+++ app/src/main/java/appewtc/masterung/friendlyexercise/MakeExercise.java
@@ -2,12 +2,26 @@ package appewtc.masterung.friendlyexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MakeExercise extends AppCompatActivity {
+
+    //Explicit
+    private String subjectString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_exercise);
+
+        //Show Subject
+        showSubject();
+
+    }   // Main Method
+
+    private void showSubject() {
+        subjectString = getIntent().getStringExtra("Subject");
+        TextView subjectTextView = (TextView) findViewById(R.id.txtTitleMake);
+        subjectTextView.setText(subjectString);
     }
-}
+}   // Main Class
